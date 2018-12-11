70f34eacfb0ad442392c3d5ffdf19aa455d18b1c
xlmnope <lmgiroud@gmail.com>
2018-12-10 19:51:32 -0800

    wins and losses calculated and shown to user, game starts another round after a win or loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,7 +18,7 @@ var numOfGemsNeeded = getRandomInt(19, 120);
 console.log(numOfGemsNeeded);
 
 //show numOfGemsNeeded in html
-document.getElementById("crystals-needed").innerHTML = "Total needed:" + numOfGemsNeeded;
+document.getElementById("crystals-needed").innerHTML = "Crystals needed: " + numOfGemsNeeded;
 
 
 //assign number to each gem randomly 
@@ -30,7 +30,13 @@ console.log("yellow" + yellowGemNumber, "green" + greenGemNumber, "red" + redGem
 
 var total = 0
 //show total right away before click events
-document.getElementById("total").innerHTML = total;
+document.getElementById("total").innerHTML = "Crystals collected: " + total;
+
+var wins = 0
+//show wins
+document.getElementById("wins").innerHTML = "Wins " + wins;
+var losses = 0
+document.getElementById("losses").innerHTML = "Losses " + losses;
 
 
 // create click events for each gem
@@ -56,15 +62,40 @@ document.getElementById("blue-gem").addEventListener("click", function(event){
 
 function updateTotal(value) {
   total += value
-  document.getElementById("total").innerHTML = "Total:" + total;
+  document.getElementById("total").innerHTML = "Crystals collected: " + total;
   if (total === numOfGemsNeeded) {
     console.log("yay you win") // make html later
+    anotherGame();
+    updateWins();
   } else if (total > numOfGemsNeeded) {
     console.log("you lose try again!") //make html later
+    anotherGame();
+    updateLosses();
   } else {
     
   }
 }
 
+function anotherGame() {
+  numOfGemsNeeded = getRandomInt(19, 120);
+  document.getElementById("crystals-needed").innerHTML = "Crystals needed: " + numOfGemsNeeded;
+  yellowGemNumber = getRandomInt(1, 12);
+  greenGemNumber = getRandomInt(1, 12);
+  redGemNumber = getRandomInt(1, 12);
+  blueGemNumber = getRandomInt(1, 12);
+  console.log("yellow" + yellowGemNumber, "green" + greenGemNumber, "red" + redGemNumber, "blue" + blueGemNumber)
+  total = 0
+  document.getElementById("total").innerHTML = "Crystals collected: " + total;
+}
+
+function updateWins() {
+  wins += 1
+  document.getElementById("wins").innerHTML = "Wins " + wins;
+}
+
+function updateLosses() {
+  losses += 1
+  document.getElementById("losses").innerHTML = "Losses " + losses;
+}
 
 
